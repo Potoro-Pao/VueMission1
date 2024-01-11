fd7a47d6dcff7382e64fa5e6cf771fb2fee77f26
Potoro-Pao <engelpao860@gmail.com>
2024-01-11 22:11:04 +0800

    改善checkToken

--- checkToken.js
+++ checkToken.js
@@ -1,15 +1,16 @@
 function getCookie(name) {
-  const cookies = document.cookie.split("; "); // 将所有的 cookie 分割成数组
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].split("="); // 将每个 cookie 分割成名称和值
-    if (cookie[0] === name) {
-      return cookie[1]; // 如果找到了对应名称的 cookie，返回它的值
+  const cookies = document.cookie.split("; ");
+
+  for (const cookie of cookies) {
+    const [key, value] = cookie.trim().split("=");
+    if (key === name) {
+      return value;
     }
   }
-  return ""; // 如果没有找到，返回空字符串
+  return "";
 }
 
 const token = getCookie("hexVueToken");
 if (!token) {
-  window.location.href = "login.html"; // 没有 token，重定向到登录页面
+  window.location.href = "login.html";
 }
