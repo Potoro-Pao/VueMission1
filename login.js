b53bd54b7727372eb5d8bc03cdbb442ddd40fa65
Potoro-Pao <engelpao860@gmail.com>
2024-01-16 21:25:21 +0800

    將登入介面改成vue

--- login.js
+++ login.js
@@ -1,25 +1,39 @@
-const loginBtn = document.querySelector("#loginBtn");
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 const url = "https://ec-course-api.hexschool.io/v2";
 const path = "potoro";
 const productAPI = `${url}/api/${path}/products/all`;
 const adminLoginAPI = `${url}/admin/signin`;
-loginBtn.addEventListener("click", () => {
-  const userValue = document.querySelector("#username");
-  const passwordValue = document.querySelector("#password");
-  const user = {
-    username: userValue.value,
-    password: passwordValue.value,
-  };
-  axios
-    .post(adminLoginAPI, user)
-    .then((res) => {
-      const { token, expired } = res.data;
-      document.cookie = `hexVueToken=${token}; expires=${new Date(expired)}`;
-      axios.defaults.headers.common["Authorization"] = token;
-      alert("恭喜你成功登入了");
-      window.location.href = "index.html";
-    })
-    .catch((err) => {
-      alert("登入失敗!");
-    });
+const app = createApp({
+  data() {
+    return {
+      user: {
+        account: null,
+        password: null,
+      },
+    };
+  },
+  methods: {
+    loginCheck() {
+      const user = {
+        username: this.user.account,
+        password: this.user.password,
+      };
+      axios
+        .post(adminLoginAPI, user)
+        .then((res) => {
+          const { token, expired } = res.data;
+          document.cookie = `hexVueToken=${token}; expires=${new Date(
+            expired
+          )}`;
+          // axios.defaults.headers.common["Authorization"] = token;
+          window.location.href = "index.html";
+          alert("恭喜你成功登入了");
+        })
+        .catch((err) => {
+          alert("登入失敗!");
+          window.location = "login.html";
+        });
+    },
+  },
 });
+app.mount("#app");
