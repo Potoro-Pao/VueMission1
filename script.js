a6c463ae729ec03c78a2b89fb236e29dbb3d61ba
Potoro-Pao <engelpao860@gmail.com>
2024-01-20 14:48:48 +0800

    新增產品的Modal處理完成

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 const app = {
   data() {
     return {
+      myModal: null,
       apiUrl: "https://vue3-course-api.hexschool.io/v2",
       apiPath: "potoro",
       products: [],
       tempProduct: {},
       selectedProduct: null,
+      newProducts: {
+        data: {
+          title: null,
+          category: null,
+          origin_price: 0,
+          price: 0,
+          unit: null,
+          description: null,
+          content: null,
+          is_enabled: 0,
+          imageUrl: null,
+          imagesUrl: [],
+        },
+      },
     };
   },
   methods: {
@@ -28,7 +43,47 @@ const app = {
           window.location = "login.html";
         });
     },
-    addProducts() {},
+    showAddProductModal() {
+      this.myModal.show();
+    },
+    resetNewProduct() {
+      this.newProducts.data = {
+        title: null,
+        category: null,
+        origin_price: 0,
+        price: 0,
+        unit: null,
+        description: null,
+        content: null,
+        is_enabled: 0,
+        imageUrl: null,
+        imagesUrl: [],
+      };
+    },
+    addProduct() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+      const hasNoProduct =
+        !this.newProducts.data.category &&
+        !this.newProducts.data.title &&
+        !this.newProducts.data.price;
+
+      if (hasNoProduct) {
+        console.log("Product information is incomplete");
+        this.myModal.hide();
+        return;
+      } else {
+        axios
+          .post(url, this.newProducts)
+          .then((res) => {
+            console.log(res.data.message);
+            this.products.push(this.newProducts.data);
+            this.myModal.hide();
+            this.resetNewProduct();
+            this.getProducts();
+          })
+          .catch((err) => console.log(err));
+      }
+    },
     getProducts() {
       const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
       axios
@@ -64,6 +119,7 @@ const app = {
     console.log(token);
 
     this.checkAdmin();
+    this.myModal = new bootstrap.Modal(document.getElementById("productModal"));
   },
 };
 
