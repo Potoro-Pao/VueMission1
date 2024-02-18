553f925ad6b6f913be394c72bc420f4981b62ce6
Potoro-Pao <engelpao860@gmail.com>
2024-02-18 17:12:43 +0800

    路由打完了

---
+++ src/views/FrontView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>這是前台</h1>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/about">About</RouterLink>|
+    <RouterLink to="/products">產品</RouterLink> |
+    <RouterLink to="/cart">購物車</RouterLink>|
+    <RouterLink to="/login">登入</RouterLink>|
+    <RouterLink to="/admin">後台</RouterLink>|
+  </nav>
+
+  <RouterView></RouterView>
+</template>
---
+++ src/views/dashboard/LoginView.vue
@@ -0,0 +1,89 @@
+<template>
+  <h2>登入Login</h2>
+  <div class="container">
+    <div class="row justify-content-center">
+      <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+      <div class="col-8">
+        <form id="form" class="form-signin" @submit.prevent="loginCheck">
+          <div class="form-floating mb-3">
+            <input
+              @keyup.enter="loginCheck"
+              v-model="user.account"
+              type="email"
+              class="form-control"
+              id="username"
+              placeholder="name@example.com"
+              required
+              autofocus
+            />
+            <label for="username">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input
+              @keyup.enter="loginCheck"
+              v-model="user.password"
+              type="password"
+              class="form-control"
+              id="password"
+              placeholder="Password"
+              required
+            />
+            <label for="password">Password</label>
+          </div>
+          <button
+            @click="loginCheck"
+            id="loginBtn"
+            class="btn btn-lg btn-primary w-100 mt-3"
+            type="button"
+          >
+            登入
+          </button>
+        </form>
+      </div>
+    </div>
+    <p class="mt-5 mb-3 text-muted">&copy; 2021~∞ - 六角學院</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const { VITE_URL } = import.meta.env;
+
+const adminLoginAPI = `${VITE_URL}/admin/signin`;
+export default {
+  data() {
+    return {
+      user: {
+        account: null,
+        password: null,
+      },
+    };
+  },
+  methods: {
+    loginCheck() {
+      const user = {
+        username: this.user.account,
+        password: this.user.password,
+      };
+      axios
+        .post(adminLoginAPI, user)
+        .then((res) => {
+          const { token, expired } = res.data;
+          document.cookie = `hexVueToken=${token}; expires=${new Date(
+            expired,
+          )}`;
+
+          this.$router.push('/admin/products');
+          // axios.defaults.headers.common["Authorization"] = token;
+          //   window.location.href = 'index.html';
+          alert('恭喜你成功登入了');
+        })
+        .catch(() => {
+          alert('登入失敗!');
+          window.location = 'login.html';
+        });
+    },
+  },
+};
+</script>
