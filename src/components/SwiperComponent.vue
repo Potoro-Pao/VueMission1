bd0d779ec125dfa21d415f7e950e05b4a9f7deca
Potoro-Pao <engelpao860@gmail.com>
2024-02-26 00:12:04 +0800

    目前還在抱錯階段

--- src/components/SwiperComponent.vue
+++ src/components/SwiperComponent.vue
@@ -1,53 +1,64 @@
 <template>
-  <swiper
-    :modules="modules"
-    :slides-per-view="3"
-    :space-between="50"
-    navigation
-    :pagination="{ clickable: true }"
-    :scrollbar="{ draggable: true }"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    ...
-  </swiper>
+  <div>
+    <swiper
+      :slides-per-view="1"
+      :space-between="50"
+      :modules="modules"
+      navigation
+      :pagination="{ clickable: true }"
+    >
+      <swiper-slide>
+        <img
+          src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80"
+          alt=""
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          src="https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1529&q=80"
+          alt=""
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          src="https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          alt=""
+        />
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 <script>
-// import Swiper core and required modules
-import {
-  Navigation, Pagination, Scrollbar, A11y,
-} from 'swiper/modules';
-
-// Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination } from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
-// Import Swiper styles
 export default {
+  data() {
+    return {
+      modules: [Navigation, Pagination],
+    };
+  },
   components: {
     Swiper,
     SwiperSlide,
   },
-  setup() {
-    const onSwiper = () => {
-      // console.log(swiper);
-    };
-    const onSlideChange = () => {
-      // console.log('slide change');
-    };
-    return {
-      onSwiper,
-      onSlideChange,
-      modules: [Navigation, Pagination, Scrollbar, A11y],
-    };
-  },
 };
 </script>
+
+<style scoped>
+.swiper {
+  width: 600px;
+  height: 400px;
+}
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
 <template>
+
+  <SwiperC></SwiperC>
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-3"><h2>See People also Buy</h2></div>
@@ -11,10 +13,12 @@
 <script>
 
 import MapC from '../components/MapComponent.vue';
+import SwiperC from '../components/SwiperComponent.vue';
 
 export default {
   components: {
     MapC,
+    SwiperC,
   },
 };
 </script>
