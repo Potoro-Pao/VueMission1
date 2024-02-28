ba29345967a4e319250d3c7de3a81e8b6c407a29
Potoro-Pao <engelpao860@gmail.com>
2024-02-28 17:20:24 +0800

    swiper加入loop

--- src/components/SwiperComponent.vue
+++ src/components/SwiperComponent.vue
@@ -6,21 +6,29 @@
       :modules="modules"
       navigation
       :pagination="{ clickable: true }"
+      :autoplay="{
+        delay: 2500,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+      }"
     >
       <swiper-slide>
         <img
+          loop
           src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80"
           alt=""
         />
       </swiper-slide>
       <swiper-slide>
         <img
+          loop
           src="https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1529&q=80"
           alt=""
         />
       </swiper-slide>
       <swiper-slide>
         <img
+          loop
           src="https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           alt=""
         />
@@ -30,16 +38,17 @@
 </template>
 <script>
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 export default {
   data() {
     return {
-      modules: [Navigation, Pagination],
+      modules: [Navigation, Pagination, Autoplay],
     };
   },
   components: {
