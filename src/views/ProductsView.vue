8771a8154ab538cc5b2e54482bf1db6d62a99f05
Potoro-Pao <engelpao860@gmail.com>
2024-02-28 14:13:50 +0800

    產品列表可切換分類

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -4,28 +4,62 @@
     <div class="row">
       <!-- 分類菜單 -->
       <div class="col-md-3">
-        <div class="list-group">
-          <a href="#" class="list-group-item list-group-item-action active">所有分類</a>
-          <a href="#" class="list-group-item list-group-item-action">分類一</a>
-          <a href="#" class="list-group-item list-group-item-action">分類二</a>
-          <a href="#" class="list-group-item list-group-item-action">分類三</a>
+        <div
+          id="collapseOne"
+          class="collapse show"
+          aria-labelledby="headingOne"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="card-body py-0">
+            <h2>Categories</h2>
+            <ul class="list-unstyled">
+              <li>
+                <router-link class="py-2 d-block text-muted" :to="`/products`"
+                  >All</router-link
+                >
+              </li>
+              <li v-for="item in categories" :key="item">
+                <router-link
+                  class="py-2 d-block text-muted"
+                  :to="`/products?categories=${item}`"
+                  >{{ item }}</router-link
+                >
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <!-- 產品卡片 -->
-      <div class="col-md-8 ">
+      <div class="col-md-8">
         <div class="row">
-          <div class="col-md-4 d-flex align-items-stretch"
-           v-for="product in products" :key="product.id">
-            <div class="card mb-3 w-100 d-flex flex-column"> <!-- 加入了flex布局 -->
-              <img  style="object-fit: cover"
-               :src="product.imageUrl" class="card-img-top" alt="產品圖片">
-              <div class="card-body d-flex flex-column"> <!-- 调整了这里 -->
+          <div
+            class="col-md-4 d-flex align-items-stretch"
+            v-for="product in products"
+            :key="product.id"
+          >
+            <div class="card mb-3 w-100 d-flex flex-column">
+              <!-- 加入了flex布局 -->
+              <img
+                style="object-fit: cover"
+                :src="product.imageUrl"
+                class="card-img-top"
+                alt="產品圖片"
+              />
+              <div class="card-body d-flex flex-column">
+                <!-- 调整了这里 -->
                 <h5 class="card-title">{{ product.title }}</h5>
-                <p class="card-text flex-grow-1">{{ product.price }} 元</p> <!-- 使用flex-grow -->
-                <div class="mt-2 mb-2"> <!-- 确保按钮在底部 -->
-                  <router-link :to="`/product/${product.id}`"
-                  class="btn btn-outline-secondary w-50 gx-3">View More</router-link>
-                  <a href="#" class="btn btn-outline-info w-50 gx-3">Add to Cart</a>
+                <p class="card-text flex-grow-1">{{ product.price }} 元</p>
+                <!-- 使用flex-grow -->
+                <div class="mt-2 mb-2">
+                  <!-- 确保按钮在底部 -->
+                  <router-link
+                    :to="`/product/${product.id}`"
+                    class="btn btn-outline-secondary w-50 gx-3"
+                    >View More</router-link
+                  >
+                  <a href="#" class="btn btn-outline-info w-50 gx-3"
+                    >Add to Cart</a
+                  >
                 </div>
               </div>
             </div>
@@ -49,12 +83,33 @@ export default {
   data() {
     return {
       isLoading: true,
+      products: [],
+      categories: [
+        'Comic',
+        'History',
+        'Young Adult',
+        'Drama',
+        'novel',
+        'Sci-Fi',
+        'Murder',
+        'TextBook',
+      ],
     };
+  },
+
+  watch: {
+    '$route.query': {
+      handler() {
+        this.getProduct();
+      },
+      deep: true,
+    },
   },
   methods: {
     getProduct() {
-      const api = `${VITE_URL}/api/${VITE_API}/products/all`;
-
+      const { categories = '' } = this.$route.query;
+      const api = `${VITE_URL}/api/${VITE_API}/products?category=${categories}`;
+      // /products?category=Drama
       axios.get(api).then((res) => {
         this.products = res.data.products;
         this.pages = res.data.pagination;
