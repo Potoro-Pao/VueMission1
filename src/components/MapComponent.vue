4ff9f8e8207448be5643b967e64df43df8010218
Potoro-Pao <engelpao860@gmail.com>
2024-03-04 00:03:36 +0800

    修正map功能location比較精確的定位以及初始化不會顯示圖片的問題

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -32,35 +32,50 @@ export default {
   },
   methods: {
     getRandom() {
-      axios.get('https://randomuser.me/api/').then((res) => {
-        this.country = res.data.results[0].location.country;
-        this.city = res.data.results[0].location.city;
-        // 调用方法获取国家的经纬度
-        this.getCountryCoordinates(this.country);
-      });
+      axios
+        .get('https://randomuser.me/api/')
+        .then((res) => {
+          if (res.data && res.data.results && res.data.results.length > 0) {
+            this.country = res.data.results[0].location.country;
+            this.city = res.data.results[0].location.city;
+            this.getCountryCoordinates(this.country);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    // 新增方法：获取国家的经纬度
+
     getCountryCoordinates(countryName) {
-      const url = `https://nominatim.openstreetmap.org/search?country=${countryName}&format=json&limit=1`;
-      axios.get(url)
+      const cityName = this.city;
+      const url = `https://nominatim.openstreetmap.org/search?country=${countryName}&city=${cityName}&format=json&limit=1`;
+      axios
+        .get(url)
         .then((response) => {
           if (response.data && response.data.length > 0) {
             const { lat, lon } = response.data[0];
-            // 更新经纬度
             this.latitude = parseFloat(lat);
             this.longitude = parseFloat(lon);
+            // 确保地图在这里初始化，确保有数据后才初始化或更新地图
             if (!this.map) {
-              this.getMap(); // 如果地图未初始化，则初始化地图
-            } else {
-              this.updateMap(); // 如果地图已初始化，则更新地图视图和标记
+              this.getMap(); // 地图未初始化，则初始化地图
             }
+            this.updateMap(); // 更新地图视图和标记
           }
+        })
+        .catch((error) => {
+          console.error('获取坐标失败:', error);
         });
     },
+
     getMap() {
-      this.map = L.map(this.$refs.mapContainer).setView([this.latitude, this.longitude], 8);
+      this.map = L.map(this.$refs.mapContainer).setView(
+        [this.latitude, this.longitude],
+        8,
+      );
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(this.map);
       this.marker = L.marker([this.latitude, this.longitude]).addTo(this.map);
       this.marker.bindPopup(`${this.city}, ${this.country}`).openPopup();
@@ -68,7 +83,17 @@ export default {
     updateMap() {
       this.map.setView([this.latitude, this.longitude], 8);
       this.marker.setLatLng([this.latitude, this.longitude]);
-      this.marker.getPopup().setContent(`${this.city}, ${this.country}`).openPopup();
+      const imageUrl = 'https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/F01/986/47/F019864769.jpg&v=64185ee5k&w=348&h=348';
+      const popupContent = `
+    <div style="display: flex; align-items: center;">
+      <div style="flex: 1;">
+        <img src="${imageUrl}" alt="Image" style="width: 100px; height: auto;">
+      </div>
+      <div style="flex: 2; margin-left: 10px;">
+        <p>SomeOne from ${this.city}, ${this.country} <br> bought Eragon with free delivery</p>
+      </div>
+    </div>`;
+      this.marker.getPopup().setContent(popupContent).openPopup();
     },
   },
 };
--- src/stores/mapStore.js
+++ src/stores/mapStore.js
@@ -1,68 +1,87 @@
-// import { defineStore } from 'pinia';
-// import L from 'leaflet';
-// import 'leaflet/dist/leaflet.css';
+// import {defineStore} from 'pinia';
 // import axios from 'axios';
 
-// // Changed from named export to default export
 // export default defineStore('mapStore', {
-//   state: () => ({
-//     map: null,
-//     marker: null,
-//     intervalId: null,
-//     country: null,
-//     city: null,
-//     latitude: null,
-//     longitude: null,
-//   }),
-//   actions: {
-//     getRandom() {
-//       axios.get('https://randomuser.me/api/').then((res) => {
-//         this.country = res.data.results[0].location.country;
-//         this.city = res.data.results[0].location.city;
-//         this.getCountryCoordinates(this.country);
-//       });
-//     },
-//     getCountryCoordinates(countryName) {
-//       const url = `https://nominatim.openstreetmap.org/search?country=${countryName}&format=json&limit=1`;
-//       axios.get(url).then((response) => {
-//         if (response.data && response.data.length > 0) {
-//           const { lat, lon } = response.data[0];
-//           this.latitude = parseFloat(lat);
-//           this.longitude = parseFloat(lon);
-//           this.updateMap();
-//         }
-//       });
-//     },
-//     initializeMap(mapContainer) {
-//       if (!this.map) {
-//         this.map = L.map(mapContainer).setView([this.latitude, this.longitude], 8);
-//         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//         }).addTo(this.map);
-//         this.marker = L.marker([this.latitude, this.longitude]).addTo(this.map);
-//       }
-//       this.updateMap();
-//     },
-//     updateMap() {
-//       if (this.map && this.latitude && this.longitude) {
-//         this.map.setView([this.latitude, this.longitude], 8);
-//         if (this.marker) {
-//           this.marker.setLatLng([this.latitude, this.longitude]);
-//           this.marker.getPopup().setContent(`${this.city}, ${this.country}`).openPopup();
-//         } else {
-//           this.marker = L.marker([this.latitude, this.longitude]).addTo(this.map);
-//           this.marker.bindPopup(`${this.city}, ${this.country}`).openPopup();
-//         }
-//       }
-//     },
-//     startLocationUpdate() {
-//       this.getRandom();
-//       this.intervalId = setInterval(this.getRandom, 10000);
-//     },
-//     stopLocationUpdate() {
-//       if (this.intervalId) {
-//         clearInterval(this.intervalId);
-//       }
-//     },
-//   },
-// });
+//     state: () =>({
+//         map: null,
+//       marker: null,
+//       intervalId: null, 
+//       country: null,
+//       city: null,
+//       latitude: null,
+//       longitude: null,
+//     }),
+//     actions: {
+//         clearIntervalId(){
+//             clearInterval(this.intervalId); 
+//         },
+//         pokeInterval(){
+//             this.intervalId = setInterval(this.getRandom, 10000); // 每10秒更新一次位置信息
+//         },
+//         getRandom() {
+//             axios
+//               .get('https://randomuser.me/api/')
+//               .then((res) => {
+//                 if (res.data && res.data.results && res.data.results.length > 0) {
+//                   this.country = res.data.results[0].location.country;
+//                   this.city = res.data.results[0].location.city;
+//                   this.getCountryCoordinates(this.country);
+//                 }
+//               })
+//               .catch((error) => {
+//                 console.log(error);
+//               });
+//           },
+      
+//           getCountryCoordinates(countryName) {
+//             const cityName = this.city;
+//             const url = `https://nominatim.openstreetmap.org/search?country=${countryName}&city=${cityName}&format=json&limit=1`;
+//             axios
+//               .get(url)
+//               .then((response) => {
+//                 if (response.data && response.data.length > 0) {
+//                   const { lat, lon } = response.data[0];
+//                   this.latitude = parseFloat(lat);
+//                   this.longitude = parseFloat(lon);
+//                   // 确保地图在这里初始化，确保有数据后才初始化或更新地图
+//                   if (!this.map) {
+//                     this.getMap(); // 地图未初始化，则初始化地图
+//                   }
+//                   this.updateMap(); // 更新地图视图和标记
+//                 }
+//               })
+//               .catch((error) => {
+//                 console.error('获取坐标失败:', error);
+//               });
+//           },
+      
+//           getMap() {
+//             this.map = L.map(this.$refs.mapContainer).setView(
+//               [this.latitude, this.longitude],
+//               8,
+//             );
+//             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//               attribution:
+//                 '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//             }).addTo(this.map);
+//             this.marker = L.marker([this.latitude, this.longitude]).addTo(this.map);
+//             this.marker.bindPopup(`${this.city}, ${this.country}`).openPopup();
+//           },
+//           updateMap() {
+//             this.map.setView([this.latitude, this.longitude], 8);
+//             this.marker.setLatLng([this.latitude, this.longitude]);
+//             const imageUrl = 'https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/F01/986/47/F019864769.jpg&v=64185ee5k&w=348&h=348';
+//             const popupContent = `
+//           <div style="display: flex; align-items: center;">
+//             <div style="flex: 1;">
+//               <img src="${imageUrl}" alt="Image" style="width: 100px; height: auto;">
+//             </div>
+//             <div style="flex: 2; margin-left: 10px;">
+//               <p>SomeOne from ${this.city}, ${this.country} <br> bought Eragon with free delivery</p>
+//             </div>
+//           </div>`;
+//             this.marker.getPopup().setContent(popupContent).openPopup();
+//           },
+//         },
+    
+// })
